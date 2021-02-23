55da678d52de1075932b3de3ba1eb8f24fa55701
katlinsantos <katlinstaubsantos@gmail.com>
2021-02-23 17:51:59 +0000

    UI Test - created SendEmail Extension replaced from JavaScript to JSON

---
+++ src/test/java/com/mychelantonacio/cqdb/uitest/SendEmail.java
@@ -0,0 +1,22 @@
+package com.mychelantonacio.cqdb.uitest;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class SendEmail {
+    public static void main(String[] args) throws InterruptedException {
+
+        //Set up testing environment
+        System.setProperty("webdriver.chrome.driver","/Users/Kät/Downloads/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://codingquestionsdb.com/");
+
+        WebElement button = driver.findElement(By.id("contact"));
+        button.click();
+        Thread.sleep(30000);
+
+        driver.quit();
+    }
+}
